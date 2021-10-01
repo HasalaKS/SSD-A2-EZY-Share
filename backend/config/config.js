aa6a5caaf31cd7fcaf6039abd5556ae12f7f6f46
Nishith <IT18140576@my.sliit.lk>
2021-10-02 02:37:19 +0530

    API Fix

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,8 @@
 const port = process.env.PORT || 8000;
 
 // facebook configurations
-const facebook_client_id = 346633970543794;
-const facebook_client_secret = "967df0d8654933450b09867f87f9601b";
+const facebook_client_id = "";
+const facebook_client_secret = "";
 const facebook_redirect_uri = "http://localhost:3000";
 
 module.exports = {
