289d621305d3a30e6e1f9a2b04f7e34ab14a855a
Nishith <IT18140576@my.sliit.lk>
2021-10-01 13:14:41 +0530

    GoogleAuth & Upload Pages

--- frontend/src/components/GoogleAuth.js
+++ frontend/src/components/GoogleAuth.js
@@ -1,5 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "reactstrap";
+import { Grid, Paper } from "@material-ui/core";
 
 export default function Googleauth(props) {
-  return <>Google Auth Page</>;
+  const paperStyle = {
+    padding: 10,
+    height: "60vh",
+    width: 700,
+    margin: "130px auto",
+  };
+
+  return (
+    <>
+      <div className={"container mt-3"} style={{ backgroundColor: "#FDFEFD" }}>
+        <Grid>
+          <Paper elevation={10} style={paperStyle}>
+            <div className={"row"}>
+              <div className={"col-md-12 text-center"}>
+                <h1 className={"mt-3"}>Hi Nishith!</h1>
+                <div className={"d-flex justify-content-center mt-4"}>
+                  <p style={{ maxWidth: "600px" }}>
+                    <i>
+                      Welcome to EZY Share! We offer free file upload, file
+                      sharing and file transfer service without any need for
+                      registration. Upload your files and share them with your
+                      friends.
+                    </i>
+                  </p>
+                </div>
+                <hr style={{ background: "#333547", width: 600 }} />
+              </div>
+            </div>
+
+            <br />
+            <div className={"row mt-4 homePageOptionWrapper"}>
+              <div className={"column mr-5 ml-5"}>
+                <Button
+                  color={"success"}
+                  style={{ width: "10rem", height: "10rem" }}
+                >
+                  <div className="p-3">
+                    <h3>
+                      <i className="fab fa-facebook" />
+                    </h3>
+                    Upload files to{" "}
+                    <div>
+                      <b>Facebook</b>
+                    </div>
+                  </div>
+                </Button>
+              </div>
+              <div className={"column mr-5"}>
+                <Button
+                  color={"warning"}
+                  style={{ width: "10rem", height: "10rem" }}
+                  onClick
+                >
+                  <div className="p-3">
+                    <h3>
+                      <i className="fab fa-google-drive"></i>
+                    </h3>
+                    Upload files to{" "}
+                    <div>
+                      <b>Google Drive</b>
+                    </div>
+                  </div>
+                </Button>
+              </div>
+              <div>
+                <Button
+                  color={"primary"}
+                  style={{ width: "10rem", height: "10rem" }}
+                >
+                  <div className="p-3">
+                    <h3>
+                      <i className="fab fa-linkedin"></i>
+                    </h3>
+                    Upload files to{" "}
+                    <div>
+                      <b>LinkedIn</b>
+                    </div>
+                  </div>
+                </Button>
+              </div>
+            </div>
+          </Paper>
+        </Grid>
+      </div>
+    </>
+  );
 }
--- frontend/src/components/UploadPage.js
+++ frontend/src/components/UploadPage.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Grid, Paper, Button } from "@material-ui/core";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Uploadpage(props) {
-  return <>Upload Page</>;
+  const paperStyle = {
+    padding: 40,
+    height: "70vh",
+    width: 400,
+    margin: "110px auto",
+  };
+
+  return (
+    <>
+      <Grid>
+        <Paper elevation={10} style={paperStyle}>
+          <div className={"row mt-3"}>
+            <div className={"col-md-12 text-center"}>
+              <img src={"../gd.png"} height="200px" alt="logo" />
+            </div>
+          </div>
+          <div className={"row mt-5"}></div>
+
+          <form onSubmit>
+            <div className="custom-file mt-4">
+              <input
+                type="file"
+                className="custom-file-input"
+                id="customFile"
+                onChange
+              />
+              <label className="custom-file-label" htmlFor="customFile">
+                Choose File
+              </label>
+            </div>
+            <input
+              type="submit"
+              value="Upload"
+              className="btn btn-danger btn-block mt-4"
+            />
+          </form>
+        </Paper>
+      </Grid>
+      <ToastContainer />
+    </>
+  );
 }
