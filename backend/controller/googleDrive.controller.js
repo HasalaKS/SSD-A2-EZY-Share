a5522e1850cd5de82aa3bd7a9ee97ed0611ac45e
IT18136784 <it18136784@my.sliit.lk>
2021-10-01 12:55:02 +0530

    import credentials and define the scope in gdrive controller

---
+++ backend/controller/googleDrive.controller.js
@@ -0,0 +1,19 @@
+const { google } = require("googleapis");
+const formidable = require("formidable");
+const fs = require("fs");
+
+// Import Client credentials
+const credentials = require("../credentials.json");
+const client_id = credentials.web.client_id;
+const client_secret = credentials.web.client_secret;
+const redirect_uris = credentials.web.redirect_uris;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+
+// Define the scope
+const SCOPE = [
+  "https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file",
+];
